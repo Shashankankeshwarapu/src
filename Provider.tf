b3d5c6d59847ad9595b3f138881e252549ebfe98
root <root@ip-172-31-92-33.ec2.internal>
2023-06-28 00:43:49 +0000

    Adding new files

---
+++ Provider.tf
@@ -0,0 +1,6 @@
+#nfo##
+provider "aws" {
+    access_key = "AKIAWB2O3I2ZBG2Q6UXY"
+    secret_key = "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"
+    region = "us-east-2"
+}
---
+++ main.tf
@@ -0,0 +1,105 @@
+ #################
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main"
+  }
+}
+ ################# Subnets #############
+resource "aws_subnet" "subnet1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+
+
+  tags = {
+    Name = "app-subnet-1"
+    }
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-2b"
+
+
+  tags = {
+    Name = "app-subnet-2"
+  }
+}
+######## IGW ###############
+resource "aws_internet_gateway" "main-igw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+########### NAT ##############
+resource "aws_eip" "nat" {
+}
+
+resource "aws_nat_gateway" "main-natgw" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id     = "${aws_subnet.subnet2.id}"
+
+  tags = {
+    Name = "main-nat"
+  }
+}
+
+############# Route Tables ##########
+
+resource "aws_route_table" "main-public-rt" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main-igw.id}"
+  }
+
+  tags = {
+    Name = "main-public-rt"
+  }
+}
+
+resource "aws_route_table" "main-private-rt" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_nat_gateway.main-natgw.id}"
+  }
+
+  tags = {
+    Name = "main-private-rt"
+  }
+}
+######### PUBLIC Subnet assiosation with route table    ######
+resource "aws_route_table_association" "public-assoc-1" {
+  subnet_id      = "${aws_subnet.subnet1.id}"
+  route_table_id = "${aws_route_table.main-public-rt.id}"
+}
+########## PRIVATE Subnets assiosation with rotute table ######
+resource "aws_route_table_association" "private-assoc-1" {
+  subnet_id      = "${aws_subnet.subnet2.id}"
+  route_table_id = "${aws_route_table.main-private-rt.id}"
+}
+
+resource "aws_key_pair" "terraform-demo" {
+  key_name   = "terraform-demo"
+  public_key = "${file("terraform-demo.pub")}"
+}
+
+resource "aws_instance" "test-instance" {
+	ami = "ami-053b0d53c279acc90"
+	instance_type = "t2.micro"
+	#key_name = "${aws_key_pair.terraform-demo.key_name}"#
+	user_data = "${file("app-install.sh")}"
+	tags = {
+		Name = "Terraform"	
+	}
+}
